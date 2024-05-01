54686b2960af8c9c9e76e31943949eeb17eab3b5
sammieatkins <104380965+sammieatkins@users.noreply.github.com>
2024-05-01 11:38:41 -0600

    images finally work wahoooo

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const static = require("./routes/static")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+// app.use(express.static("./public"));
 
 /* ***********************
  * Routes
